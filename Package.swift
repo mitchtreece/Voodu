7ba4d61fafe231991833c8ddb7360a4cf260e512
Mitch Treece <mitchtreece@me.com>
2022-08-15 15:38:18 -0700

    Added placeholder spm package file

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 5.6
+
+import PackageDescription
+
+let package = Package(
+
+    name: "Voodu",
+
+    platforms: [
+
+        .iOS(.v13)
+
+    ],
+
+    products: [
+
+        .library(
+            name: "Voodu",
+            targets: ["Voodu"]
+        )
+
+    ],
+
+    targets: [
+
+        .target(
+            name: "Voodu",
+            path: "Voodu/Classes",
+        )
+
+    ],
+
+)
